708bdc14c713e9f846b536e97dd369d7059cd3a2
c4ffein <c4ffein>
2021-11-19 06:20:29 +0100

    updated setup

--- setup.py
+++ setup.py
@@ -1,13 +1,35 @@
 from setuptools import setup
 
 setup(
-    name='mik',
-    version='0.0.1',
-    description='KISS 1 command deploy',
-    long_description='',
-    url='https://github.com/c4ffein/mik',
-    author='c4ffein',
-    author_email='c4ffein@gmail.com',
+    name="mik",
+    version="0.0.2",
+    description="KISS 1 command deploy",
+    long_description="",
+    url="https://github.com/c4ffein/mik",
+    author="c4ffein",
+    author_email="c4ffein@gmail.com",
     packages=[],
-    classifiers=['Development Status :: 1 - Planning'],
+    classifiers=["Development Status :: 1 - Planning",
+        "Intended Audience :: Developers"
+        "Intended Audience :: Education"
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Other Audience",
+        "Intended Audience :: Science/Research",
+        "Intended Audience :: System Administrators"
+        "License :: OSI Approved :: MIT License",
+        "Environment :: Console",
+        "Operating System :: MacOS",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Office/Business",
+        "Topic :: Utilities",
+    ],
+    license="MIT",
 )
