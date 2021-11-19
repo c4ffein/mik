2d914a410f2701b58bbb94c75cdc2fa2489c2693
c4ffein <c4ffein>
2021-11-19 06:09:08 +0100

    planning to clean

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='mik',
+    version='0.0.1',
+    description='KISS 1 command deploy',
+    long_description='',
+    url='https://github.com/c4ffein/mik',
+    author='c4ffein',
+    author_email='c4ffein@gmail.com',
+    packages=[],
+    classifiers=['Development Status :: 1 - Planning'],
+)
